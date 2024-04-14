06a3b7dae3c335e00c07d0729c0e8bb27bd2c498
Andres Benavides <andresbena3515@gmail.com>
2024-04-13 20:53:31 -0500

    añadido seccion de lenguajes, proyectos y footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,14 @@ import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { PerfilComponent } from './components/perfil/perfil.component';
 import { EducacionComponent } from './components/educacion/educacion.component';
+import { HerramientasComponent } from './components/herramientas/herramientas.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { ProyectosComponent } from './components/proyectos/proyectos.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, PerfilComponent, EducacionComponent],
+  imports: [RouterOutlet, NavbarComponent, PerfilComponent, EducacionComponent, HerramientasComponent, FooterComponent, ProyectosComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
