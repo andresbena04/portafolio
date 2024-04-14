bd9335aa3aa1f0967c76747d3c38ea26566d4539
Andres Benavides <andresbena3515@gmail.com>
2024-04-13 22:48:42 -0500

    btn donwload cv

--- src/app/components/contacto/contacto.component.ts
+++ src/app/components/contacto/contacto.component.ts
@@ -9,5 +9,17 @@ import { Component } from '@angular/core';
 })
 export class ContactoComponent {
   
-  
+  donwload(): void {
+    
+    const imageUrl = 'assets/CV_Andres_Benavides.pdf';
+    const fileName = 'CV_Andres_Benavides.pdf';
+
+    // Crear un enlace temporal
+    const link = document.createElement('a');
+    link.href = imageUrl;
+    link.download = fileName;
+
+    // Simular un clic en el enlace para iniciar la descarga
+    link.click();
+  }
 }
