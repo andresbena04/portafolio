0de49ed88c3343d602b7bb48c0ef20610d5fa948
Andres Benavides <andresbena3515@gmail.com>
2024-04-13 21:51:14 -0500

    Scroll a una sección específica

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { ProyectosComponent } from './components/proyectos/proyectos.component';
+import { PerfilComponent } from './components/perfil/perfil.component';
+import { HerramientasComponent } from './components/herramientas/herramientas.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'perfil', component: PerfilComponent },
+    { path: 'lenguajes', component: HerramientasComponent },
+    { path: 'proyectos', component: ProyectosComponent }
+];
--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,12 +1,28 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
 
+  constructor(private router: Router) { }
+
+  ngOnInit() { }
+  scrollToElement(elementId: string, route: string): void {
+    const element = document.getElementById(elementId);
+    if (element) {
+      element.scrollIntoView({ behavior: 'smooth', block: 'start', inline: 'nearest' });
+      console.log(route)
+      this.router.navigateByUrl(route);
+    }
+  }
+  isActive(route: string): boolean {
+    return this.router.url === route;
+  }
 }
