ff51768576248bd2d226c0ce4184b8632d598b94
Andres Benavides <andresbena3515@gmail.com>
2024-04-21 18:19:55 -0500

    cambio de img de proyecto cine

--- src/app/components/proyectos/proyectos.component.ts
+++ src/app/components/proyectos/proyectos.component.ts
@@ -1,9 +1,10 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-proyectos',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './proyectos.component.html',
   styleUrl: './proyectos.component.css'
 })
@@ -17,10 +18,13 @@ export class ProyectosComponent {
       {
         titulo:"Cine App",
         descripcion:"Diseño de una aplicacion web para un cine, con la vista de las peliculas que estan en cartera y la que estan proximas a estrenar",
-        img:"assets/img/Cine-app.png",
+        img:["assets/img/cine1.png","assets/img/cine2.png"],
         tecnologias:["Angular", "Bootstrap"],
         url:"https://andresbena04.github.io/cine-angular/"
       }
     ]
+  }
+  MultipleProjects(): boolean {
+    return this.proyectos.length > 1;
   }
 }
